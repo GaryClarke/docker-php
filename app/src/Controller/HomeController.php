8cd4e81290f5fc65274f069449ab3bf6a51364a5
Gary Clarke <info@garyclarke.tech>
2022-12-13 14:44:14 +0000

    Add Symfony skeleton files

---
+++ app/src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function home(): Response
+    {
+        return new Response();
+    }
+}
